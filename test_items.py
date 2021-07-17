945b7126fb5e67992754d546e68d20f1fabbdb51
bushuev-alex <bushuev-alex@mail.ru>
2021-07-17 15:56:03 +0700

    Add working solution.

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ import time
 from selenium import webdriver
 
 
-def test_page_contains_button_add_to_card(browser):
+def test_page_contains_button_add_to_cart(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
     time.sleep(3)
