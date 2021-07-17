8ecdd3de32a749e431af1ce790c95075290eeec9
bushuev-alex <bushuev-alex@mail.ru>
2021-07-17 15:27:41 +0700

    Add working solution.

--- conftest.py
+++ conftest.py
@@ -1,19 +1,26 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="en",
+                     help="Select a language")
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
+    browser_lang = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': f'{browser_lang}'})
+
     browser = None
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+        browser = webdriver.Chrome(chrome_options=options)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
         browser = webdriver.Firefox(firefox_profile='/home/alexander/.mozilla/firefox/ammke3ya.default')
@@ -22,5 +29,4 @@ def browser(request):
     yield browser
     print("\nquit browser..")
     browser.quit()
-
 
---
+++ test_items.py
@@ -0,0 +1,12 @@
+import pytest
+import time
+from selenium import webdriver
+
+
+def test_page_contains_button_add_to_card(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    browser.get(link)
+    time.sleep(3)
+    button_add_to_card = browser.find_element_by_css_selector("button.btn")
+    time.sleep(3)
+    assert button_add_to_card, "button is missing"
