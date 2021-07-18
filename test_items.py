57e3decd7e816d0a41b47d9424879659fd4acf62
bushuev-alex <bushuev-alex@mail.ru>
2021-07-18 11:47:43 +0700

    Replace unique selector

--- test_items.py
+++ test_items.py
@@ -1,12 +1,10 @@
-import pytest
 import time
-from selenium import webdriver
 
 
 def test_page_contains_button_add_to_cart(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
-    time.sleep(3)
-    button_add_to_card = browser.find_element_by_css_selector("button.btn")
-    time.sleep(3)
+    time.sleep(4)
+    button_add_to_card = browser.find_element_by_css_selector("#add_to_basket_form .btn-add-to-basket")
+    time.sleep(4)
     assert button_add_to_card, "button is missing"
