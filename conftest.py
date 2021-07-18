1d7af27f6b0d85375d3499953e559bac31a9e87f
bushuev-alex <bushuev-alex@mail.ru>
2021-07-18 11:56:17 +0700

    Add Firefox profile

--- conftest.py
+++ conftest.py
@@ -20,10 +20,12 @@ def browser(request):
     browser = None
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome(chrome_options=options)
+        browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox(firefox_profile='/home/alexander/.mozilla/firefox/ammke3ya.default')
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", browser_lang)
+        browser = webdriver.Firefox(firefox_profile=fp)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
